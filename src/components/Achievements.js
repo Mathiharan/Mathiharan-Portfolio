7cda9370da6f3ee8351674a17eb17ffb78acfa8d
Mathiharan <sunmathi2@gmail.com>
2021-04-10 01:20:45 +0530

    Badges and Skills ready

--- src/components/Achievements.js
+++ src/components/Achievements.js
@@ -47,50 +47,76 @@ function Achievements(props) {
   );
 }
 
-const app = document.getElementById('app');
-const data = [{
-  id: 1,
-  name: "Island",
-  image: "https://images.unsplash.com/photo-1442530792250-81629236fe54?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=980&h=980&fit=crop&s=9631adb2d2f752e3a0734f393fef634b"
-}, {
-  id: 2,
-  name: "Forest",
-  image: "https://images.unsplash.com/photo-1468851508491-4f854ec88aa0?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=b1222b6a1d3694cac76d2a23c3a02254"
-}, {
-  id: 3,
-  name: "Whale",
-  image: "https://images.unsplash.com/photo-1454991727061-be514eae86f7?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=3c55430f01fe9ac9a9ccb3383d1416ff"
-}, {
-  id: 4,
-  name: "Mountain",
-  image: "https://images.unsplash.com/photo-1467890947394-8171244e5410?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=980&h=980&fit=crop&s=9396f0adf263b51b44626228225684d0"
-}, {
-  id: 5,
-  name: "Boat",
-  image: "https://images.unsplash.com/photo-1443302382600-0bfacc473376?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=980&h=980&fit=crop&s=0c0f26518c1001f67b6c2e4480a8d3e0"
-}, {
-  id: 6,
-  name: "Flowers",
-  image: "https://images.unsplash.com/photo-1429091443922-e7d9ae79a837?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=e81cb6a60c53788559edb9bec21b80fc"
-}, {
-  id: 7,
-  name: "Fire",
-  image: "https://images.unsplash.com/photo-1468245856972-a0333f3f8293?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=1f57cc13084e32839627453821a43abf"
-}, {
-  id: 8,
-  name: "Garden",
-  image: "https://images.unsplash.com/photo-1427392797425-39090deb14ec?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=8bfe49466d0da200e61128a8ab0e8fbe"
-}, {
-  id: 9,
-  name: "Bridge",
-  image: "https://images.unsplash.com/photo-1445723356089-6dbb51d9c4f8?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=980&h=980&fit=crop&s=6e476c6e7ce1adac161295616d1bec05"
-}];
+const app = document.getElementById("app");
+const data = [
+  {
+    id: 1,
+    name: "Island",
+    heading: "Volleyball",
+    image:
+      "https://images.unsplash.com/photo-1442530792250-81629236fe54?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=980&h=980&fit=crop&s=9631adb2d2f752e3a0734f393fef634b",
+  },
+  {
+    id: 2,
+    name: "Forest",
+    heading: "Volleyball",
+    image:
+      "https://images.unsplash.com/photo-1468851508491-4f854ec88aa0?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=b1222b6a1d3694cac76d2a23c3a02254",
+  },
+  {
+    id: 3,
+    name: "Whale",
+    heading: "Volleyball",
+    image:
+      "https://images.unsplash.com/photo-1454991727061-be514eae86f7?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=3c55430f01fe9ac9a9ccb3383d1416ff",
+  },
+  {
+    id: 4,
+    name: "Mountain",
+    heading: "Volleyball",
+    image:
+      "https://images.unsplash.com/photo-1467890947394-8171244e5410?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=980&h=980&fit=crop&s=9396f0adf263b51b44626228225684d0",
+  },
+  {
+    id: 5,
+    name: "Boat",
+    heading: "Volleyball",
+    image:
+      "https://images.unsplash.com/photo-1443302382600-0bfacc473376?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=980&h=980&fit=crop&s=0c0f26518c1001f67b6c2e4480a8d3e0",
+  },
+  {
+    id: 6,
+    name: "Flowers",
+    heading: "Volleyball",
+    image:
+      "https://images.unsplash.com/photo-1429091443922-e7d9ae79a837?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=e81cb6a60c53788559edb9bec21b80fc",
+  },
+  {
+    id: 7,
+    name: "Fire",
+    heading: "Volleyball",
+    image:
+      "https://images.unsplash.com/photo-1468245856972-a0333f3f8293?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=1f57cc13084e32839627453821a43abf",
+  },
+  {
+    id: 8,
+    name: "Garden",
+    heading: "Volleyball",
+    image:
+      "https://images.unsplash.com/photo-1427392797425-39090deb14ec?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=8bfe49466d0da200e61128a8ab0e8fbe",
+  },
+  {
+    id: 9,
+    name: "Bridge",
+    heading: "Volleyball",
+    image:
+      "https://images.unsplash.com/photo-1445723356089-6dbb51d9c4f8?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=980&h=980&fit=crop&s=6e476c6e7ce1adac161295616d1bec05",
+  },
+];
 
 class App extends React.Component {
   render() {
-    return (
-      <Tiles data={this.props.data} />
-    );
+    return <Tiles data={this.props.data} />;
   }
 }
 
@@ -101,7 +127,7 @@ class Tiles extends React.Component {
     return (
       <div className="tiles">
         {this.props.data.map((data) => {
-          return <Tile data={data} key={data.id} />
+          return <Tile data={data} key={data.id} />;
         })}
       </div>
     );
@@ -110,43 +136,43 @@ class Tiles extends React.Component {
 
 class Tile extends React.Component {
   constructor(props) {
-      super(props);
-      this.state = {
-        open: false,
-        mouseOver: false
-      };
-      // Bind properties to class instance
-      this._clickHandler = this._clickHandler.bind(this);
-      this._mouseEnter = this._mouseEnter.bind(this);
-      this._mouseLeave = this._mouseLeave.bind(this);
-    }
-    // Event handlers to modify state values
+    super(props);
+    this.state = {
+      open: false,
+      mouseOver: false,
+    };
+    // Bind properties to class instance
+    this._clickHandler = this._clickHandler.bind(this);
+    this._mouseEnter = this._mouseEnter.bind(this);
+    this._mouseLeave = this._mouseLeave.bind(this);
+  }
+  // Event handlers to modify state values
   _mouseEnter(e) {
     e.preventDefault();
     if (this.state.mouseOver === false) {
       console.log(this.props.data.name);
       this.setState({
-        mouseOver: true
-      })
+        mouseOver: true,
+      });
     }
   }
   _mouseLeave(e) {
     e.preventDefault();
     if (this.state.mouseOver === true) {
       this.setState({
-        mouseOver: false
-      })
+        mouseOver: false,
+      });
     }
   }
   _clickHandler(e) {
     e.preventDefault();
     if (this.state.open === false) {
       this.setState({
-        open: true
+        open: true,
       });
     } else {
       this.setState({
-        open: false
+        open: false,
       });
     }
   }
@@ -159,21 +185,21 @@ class Tile extends React.Component {
     // When tile clicked
     if (this.state.open) {
       tileStyle = {
-        width: '62vw',
-        height: '62vw',
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        margin: '0',
-        marginTop: '-31vw',
-        marginLeft: '-31vw',
-        boxShadow: '0 0 40px 5px rgba(0, 0, 0, 0.3)',
-        transform: 'none'
+        width: "62vw",
+        height: "62vw",
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        margin: "0",
+        marginTop: "-31vw",
+        marginLeft: "-31vw",
+        boxShadow: "0 0 40px 5px rgba(0, 0, 0, 0.3)",
+        transform: "none",
       };
     } else {
       tileStyle = {
-        width: '18vw',
-        height: '18vw'
+        width: "18vw",
+        height: "18vw",
       };
     }
 
@@ -187,6 +213,12 @@ class Tile extends React.Component {
           alt={this.props.data.name}
           style={tileStyle}
         />
+        <h4>{this.props.data.heading}</h4>
+        <figure class="figure">
+          <figcaption class="figure-caption text-right">
+            A caption for the above image.
+          </figcaption>
+        </figure>
       </div>
     );
   }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -11,6 +11,7 @@ import {
   CardHeader,
   Breadcrumb,
   BreadcrumbItem,
+  Badge,
   Button,
   Row,
   Label,
@@ -21,6 +22,92 @@ import { Link } from "react-router-dom";
 import { Loading } from "./LoadingComponent";
 import { baseUrl } from "../shared/baseUrl";
 import { FadeTransform } from "react-animation-components";
+
+let data = [
+  {
+      name: 'Tab001',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores nihil, nisi, voluptate ad quis ea omnis quidem minima fugit adipisci, porro ut velit officiis natus eligendi autem inventore dolor fuga unde nesciunt expedita, beatae officia nostrum labore. Reiciendis, commodi adipisci eius est recusandae ipsa incidunt repellat explicabo nobis corporis debitis non ullam, eos itaque, quia, iste repudiandae. Iusto numquam consectetur quo, et, quis deleniti ipsam eaque perferendis. Repellat ad, molestiae id deserunt praesentium distinctio similique nesciunt itaque. Repellat error enim blanditiis esse, odio commodi exercitationem nostrum perferendis veniam quod, recusandae provident aspernatur aliquam placeat odit cumque fugit ducimus, voluptatibus ad?'
+  },
+  {
+      name: 'Tab002',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores perspiciatis repellat soluta dolorum, quam quos possimus atque rerum porro voluptate beatae dolor incidunt! Corporis, tempore quasi fugit est. Ex, quae!Aliquam nulla explicabo facilis, consequuntur tenetur! Rem architecto veritatis quo corporis sapiente nesciunt culpa at enim similique officiis adipisci in commodi suscipit, natus facilis, repellat laboriosam eaque obcaecati quaerat vero!'
+  },
+  {
+      name: 'Tab003',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem minus similique architecto sequi delectus non, nobis consequuntur officia, laboriosam reiciendis ea! Natus iste quas perspiciatis magnam repellat, voluptate excepturi esse.'
+  }
+];
+
+class Tabs extends React.Component {
+  constructor() {
+      super();
+
+      this.state = {
+          activeTab: 0,
+          data: data
+      }
+
+      this.changeTabOnClick = this.changeTabOnClick.bind(this);
+  }
+
+  changeTabOnClick(index) {
+      this.setState({
+          activeTab: index
+      });
+  }
+
+  render() {
+      return (
+          <div className="tabs-body">
+              <TabHeader data={this.state.data}
+                            click={this.changeTabOnClick}
+                            activeId={this.state.activeTab} />
+              <TabContent data={this.state.data}
+                             activeId={this.state.activeTab} />
+          </div>
+      )
+  }
+}
+
+class TabHeader extends React.Component {
+  doClick(index, event) {
+      this.props.click(index);
+  }
+
+  render() {
+      let activeClass = this.props.activeId;
+
+      let tabs = this.props.data.map((item, index) => {
+          return <li className={(activeClass === index ? 'active' : '')}>
+                      <a onClick={this.doClick.bind(this, index)} ><span>{item.name}</span></a>
+                  </li>
+      });
+
+      return (
+          <ul className="tabs-header">{tabs}</ul>
+      )
+  }
+
+}
+
+class TabContent extends React.Component {
+  render() {
+      let activeClass = this.props.activeId;
+
+      let content = this.props.data.map((item, index) => {
+          return <div  className={'tabs-textItem ' + (activeClass === index ? 'show' : '')} ><p>{item.text}</p></div>
+      });
+
+      return (
+          <div className="tabs-content">{content}</div>
+      );
+  }
+}
+
+//React.render(<Tabs />, document.querySelector('.tabs'));
+
+
+
 
 function Skills(props) {
   return (
@@ -34,12 +121,23 @@ function Skills(props) {
             <BreadcrumbItem active>Skills and Projects</BreadcrumbItem>
           </Breadcrumb>
           <div className="col-12">
-            <h3>Skills</h3>
-            <hr className="hr"/>
+            <h3>My Skills</h3>
+            <hr className="hr" />
           </div>
           <div className="row row-content">
-            <div className="col-12">
-              
+            <div className="col-12 col-md-8">
+              <Tabs />
+            </div>
+            <div className="col-md-1">
+              <hr className="verthr"/>
+            </div>
+            <div className="col-12 col-md">
+              <Badge variant="primary" className="badge">Primary</Badge>
+              <Badge variant="primary" >Primary</Badge>
+              <Badge variant="primary">Primary</Badge>
+              <Badge variant="primary">Primary</Badge>
+              <Badge variant="primary">Primary</Badge>
+              <Badge variant="primary">Primary</Badge>
             </div>
           </div>
         </div>
