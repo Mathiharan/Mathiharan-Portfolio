0fe6f2bad164e37ac67969913db59a2c8f54998d
Mathiharan <sunmathi2@gmail.com>
2021-08-02 22:22:53 +0530

    Dp Change

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -84,7 +84,7 @@ class Header extends Component {
             <div className="row row-header">
               <div className="col-12 col-md-4 ">
                 <Image
-                  src="../assets/mat.png"
+                  src="../assets/Portf1.png"
                   style={{
                     width: 250,
                     height: 250,
